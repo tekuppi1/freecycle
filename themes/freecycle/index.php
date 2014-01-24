85b7e8abdaac42521300d939eeba33005254e2e9
masabata <k.masaki.0011@gmail.com>
2014-01-24 18:15:25 +0900

    other list img height change

--- themes/freecycle/index.php
+++ themes/freecycle/index.php
@@ -28,7 +28,7 @@
 						
 							<div class="entry">	
 								
-								<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(150, 150)) ?></a>					
+								<a href="<?php the_permalink(); ?>" class="post-img-contents"><?php the_post_thumbnail(array(150, 150)) ?></a>					
 								<?php wp_link_pages( array( 'before' => '<div class="page-link"><p>' . __( 'Pages: ', 'buddypress' ), 'after' => '</p></div>', 'next_or_number' => 'number' ) ); ?>
 								<span class="index-item-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></span>
 							</div>							
