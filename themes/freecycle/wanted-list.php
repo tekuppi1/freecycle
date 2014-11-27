d161263313b6369ba9aecc58a711e175441e4a74
tagasoichiro <stitow14@gmail.com>
2014-11-27 19:32:15 +0900

    delete wanted_list department #93

--- themes/freecycle/wanted-list.php
+++ themes/freecycle/wanted-list.php
@@ -25,10 +25,6 @@
 	
 	<input type="button" name="btn_search" value="検索" onClick="onClickSearchWantedList(0,document.getElementById('keyword').value);" >
 	</li>
-	<li>
-	<label for='department'>学部で検索</label>
-	<select name='department' id='department' onChange="onClickSearchWantedList(0)"><?php echo get_department_options(); ?></select>
-	</li>
 </ul>
 <div id="wanted_list" style="height:auto !important;">
 <div align=center><img src="<?php echo get_stylesheet_directory_uri() ?>/images/ajax-loader.gif"></div>
