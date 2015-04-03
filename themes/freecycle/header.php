644d6cbe17fd2a3b2bcf25387abb997327d9ea55
beak <k.hashikuchi@gmail.com>
2015-04-03 15:13:29 +1300

    Enhance facebook like dialog #154 Show a dialog to ask users who logined with Facebook account to "like". This dialog is shown only once.

--- themes/freecycle/header.php
+++ themes/freecycle/header.php
@@ -8,7 +8,14 @@
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<?php bp_head(); ?>
 		<?php wp_head(); ?>
-		<?php include_once "js/freecycleJS.php" ?>
+		<?php
+		// if facebook dialog has not shown before, show it
+		if(is_user_connected_with('facebook', get_current_user_id()) && !get_user_meta(get_current_user_id(), 'is_fb_share_popup_displayed')){
+			// change status
+			update_user_meta(get_current_user_id(), 'is_fb_share_popup_displayed', 1);
+			include_once "js/fcFbDialog.js.php";
+		}
+		?>
 		<?php
 		wp_register_script(
 			'flexSliderScript',
