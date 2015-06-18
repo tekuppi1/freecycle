86462e12a04d4b657fe3b1c4e7cdfb011f80b251
tagasoichiro <stitow14@gmail.com>
2015-06-18 17:06:51 +0900

    enhance searchform page #209

--- themes/freecycle/search-page.php
+++ themes/freecycle/search-page.php
@@ -1,2 +1,40 @@
+<h3>キーワード検索</h3>
+
 <?php
    get_search_form();
+?>
+
+<h3>カテゴリ検索</h3>
+
+<?php
+   $main_categories = get_categories(array(
+      "parent" => 0,
+      "hide_empty" => 0,
+      "exclude" => 1 //'uncategorized'
+   ));
+
+   foreach ((array)$main_categories as $main_category) {
+      $main_id = $main_category->term_id;
+      $main_name = $main_category->name;
+      echo "<div class='main_categories' onclick='switchDisplay($main_id);'>$main_name</div>";
+
+      $sub_categories = get_categories(array(
+         "parent" => $main_id
+      ));
+
+      echo "<div class='sub_categories' id=sub_category_$main_id >";
+
+      foreach((array)$sub_categories as $sub_category){
+         $sub_name = $sub_category->name;
+         echo "<div><a href='". home_url() ."/archives/category/".$main_name."/".$sub_name ."'>" .$sub_name. "</a></div>";
+      }
+
+      echo "</div>";
+   }
+?>
+
+<script>
+   function switchDisplay(id){
+      jQuery("#sub_category_" + id).toggle();
+   }
+</script>
