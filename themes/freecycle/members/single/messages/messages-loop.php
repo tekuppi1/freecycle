50236a4a0cad47e2e2a3d481cfc325a4d2b1df1c
beak <k.hashikuchi@gmail.com>
2015-07-04 20:03:51 +1200

    Modify the style of the message list #210 Delete checkboxes and unread count, fix the translation of message count

--- themes/freecycle/members/single/messages/messages-loop.php
+++ themes/freecycle/members/single/messages/messages-loop.php
@@ -22,28 +22,26 @@
 		<?php while ( bp_message_threads() ) : bp_message_thread(); ?>
 
 			<tr id="m-<?php bp_message_thread_id(); ?>" class="<?php bp_message_css_class(); ?><?php if ( bp_message_thread_has_unread() ) : ?> unread<?php else: ?> read<?php endif; ?>">
-				<td width="1%" class="thread-count">
+<!-- 				<td width="1%" class="thread-count">
 					<span class="unread-count"><?php bp_message_thread_unread_count(); ?></span>
-				</td>
+				</td> -->
 				<td width="1%" class="thread-avatar"><?php bp_message_thread_avatar(); ?></td>
 
 				<?php if ( 'sentbox' != bp_current_action() ) : ?>
-					<td width="30%" class="thread-from">
+					<td width="50%" class="thread-from">
 						<?php _e( 'From:', 'buddypress' ); ?> <?php bp_message_thread_from(); ?><br />
+						<p>タイトル:<a href="<?php bp_message_thread_view_link(); ?>" title="<?php _e( "View Message", "buddypress" ); ?>"><?php bp_message_thread_subject(); ?></a></p>
 						<span class="activity"><?php bp_message_thread_last_post_date(); ?></span>
 					</td>
 				<?php else: ?>
-					<td width="25%" class="thread-from">
+					<td width="50%" class="thread-from">
 						<?php _e( 'To:', 'buddypress' ); ?> <?php bp_message_thread_to(); ?><br />
+						<a href="<?php bp_message_thread_view_link(); ?>" title="<?php _e( "View Message", "buddypress" ); ?>"><?php bp_message_thread_subject(); ?></a>
 						<span class="activity"><?php bp_message_thread_last_post_date(); ?></span>
 					</td>
 				<?php endif; ?>
-				<td width="55%" class="thread-info"> <!-- 暫定的なクラス -->
-					<a href="<?php bp_message_thread_view_link(); ?>" title="<?php _e( "View Message", "buddypress" ); ?>"><?php bp_message_thread_subject(); ?></a>
-				</td>
 				<?php do_action( 'bp_messages_inbox_list_item' ); ?>
-				<td width="13%" class="thread-options">
-					<input type="checkbox" name="message_ids[]" value="<?php bp_message_thread_id(); ?>" />
+ 				<td width="13%" class="thread-options">
 					<a class="button-confirm" href="<?php bp_message_thread_delete_link(); ?>" title="<?php _e( "Delete Message", "buddypress" ); ?>"><?php _e( 'Delete', 'buddypress' ); ?></a> &nbsp;
 				</td>
 			</tr>
@@ -51,10 +49,10 @@
 		<?php endwhile; ?>
 	</table><!-- #message-threads -->
 
-	<div class="messages-options-nav">
+<!-- 	<div class="messages-options-nav">
 		<?php bp_messages_options(); ?>
 	</div><!-- .messages-options-nav -->
-
+ 
 	<?php do_action( 'bp_after_member_messages_threads' ); ?>
 
 	<?php do_action( 'bp_after_member_messages_options' ); ?>
