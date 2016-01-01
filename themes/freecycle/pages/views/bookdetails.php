f764672434dd7639fe0ce0db67d60f1deb35a123
yugogura <jigenbakdaaan@gmail.com>
2016-01-01 21:20:53 +0900

    checkout

---
+++ themes/freecycle/pages/views/bookdetails.php
@@ -0,0 +1,42 @@
+
+<?php get_header(); ?>
+
+<!-------個別商品情報-------->	
+<div class="fake">
+	<div class="bookinfo">
+		<p class="booktitle">タイトル名</p>
+		<p class="bookauthor">著者名</p>
+		<img class="picture" src="/image/sc.png" width="50%" height="50%" alt="本の写真">
+		<div class="fake">
+			<table class="booksubinfo">
+				<tr>
+				<th class="normal">カテゴリー</th><td class="normal">ナゴヤ大学/情報文化学部</td>
+				</tr>
+
+				<tr>
+				<th class="normal">ポイント数</th><td class="normal">20pt</td>
+				</tr>
+
+				<tr>
+				<th class="normal">元値</th><td class="normal">20000</td>
+				</tr>
+
+				<tr>
+				<th class="empty"></th><td class="normal">残り三冊</td>
+				</tr>
+				
+			</table>
+		</div>
+	</div>
+</div>
+<div class="reserve">
+	<span class="this">この商品を</span><a class="button" href="#">予約する</a>
+	
+
+</div>	
+	
+<div class="plus">
+<P>補足情報</P>	
+</div>	
+	
+<?php get_footer(); ?>
