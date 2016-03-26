f402823b4fa4805ef6ce2ea04340ccf246b05565
yugogura <jigenbakdaaan@gmail.com>
2016-03-26 15:08:25 +0900

    add function that: only when users are logged in, display logout button.

--- themes/freecycle/index.php
+++ themes/freecycle/index.php
@@ -223,11 +223,15 @@ jQuery(window).on('load resize', function(){
 <div style="clear:both"></div>
 <!------------------------------------------------------>
 <!--ログアウト処理-->
+	<?php if( is_user_logged_in() ): ?>
+	
 <div class="index_profile">
 	<?php bp_loggedin_user_avatar( 'type=thumb&width=30&height=30' ); ?>
 	<span class="user-nicename"><?php echo bp_core_get_userlink( bp_loggedin_user_id() ); ?></span>
 	<a class="button logout" href="<?php echo wp_logout_url( wp_guess_url() ); ?>"><?php _e( 'Log Out', 'buddypress' ); ?></a>
 </div>
+	
+	<?php endif; ?>
 <!--ログアウト処理-->
 
 <?php get_footer(); ?>
