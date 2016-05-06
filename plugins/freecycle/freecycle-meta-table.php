e2ed745bd0d246f4d0139f63d7c4068a8c46af8b
hatimitusiropp <0rb3v02778z5y5h@ezweb.ne.jp>
2016-05-06 14:51:19 +0900

    up version

--- plugins/freecycle/freecycle-meta-table.php
+++ plugins/freecycle/freecycle-meta-table.php
@@ -30,7 +30,7 @@ class FreecycleMetaTable {
 	function fmt_activate(){
 		global $wpdb;
 		//DB version
-		$fmt_db_version = '2.11';
+		$fmt_db_version = '2.111';
 		//current DB version
 		$installed_ver = get_option( 'fmt_meta_version' );
 			// if versions are different tables are created
