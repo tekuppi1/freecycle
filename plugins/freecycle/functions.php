fef199499e9f2b9882434784f2d918f308f59a9f
k <k0uta120u@icloud.com>
2016-05-07 17:36:15 +0900

    made get_bookfair_info_by_venue work with new-structured db

--- plugins/freecycle/functions.php
+++ plugins/freecycle/functions.php
@@ -29,8 +29,8 @@ add_action('delete_user', 'on_user_deleted');
 remove_filter( 'bp_get_the_profile_field_value', 'xprofile_filter_link_profile_data', 9, 2);
 add_action('wp_ajax_get_bookfair_info_of_all_by_ajax', 'get_bookfair_info_of_all_by_ajax');
 add_action('wp_ajax_nopriv_get_bookfair_info_of_all_by_ajax', 'get_bookfair_info_of_all_by_ajax');
-add_action('wp_ajax_get_bookfair_info_all_you_want', 'get_bookfair_info_all_you_want');
-add_action('wp_ajax_nopriv_get_bookfair_info_all_you_want', 'get_bookfair_info_all_you_want');
+add_action('wp_ajax_get_bookfair_info_by_venue', 'get_bookfair_info_by_venue');
+add_action('wp_ajax_nopriv_get_bookfair_info_by_venue', 'get_bookfair_info_by_venue');
 
 // load files
 require_once('functions.kses.php');
@@ -3046,6 +3046,8 @@ add_shortcode('show_all_items', 'show_all_items');
 
 // 古本市開催場所から古本市id,古本市の開始日時、終了日時、開催場所、を取ってくる
 function get_bookfair_info_by_venue($bookfair_venue){
+    $bookfair_venue = "Nagoya University";
+    
 	global $wpdb;
 	global $table_prefix;
 	$sql = "SELECT " . $table_prefix . "fmt_book_fair.bookfair_id,starting_time, ending_time, venue, classroom
