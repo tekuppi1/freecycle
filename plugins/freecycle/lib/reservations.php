88397b72622957c95f0f23709014f6eed72409d9
Kotaro <k0uta120u@icloud.com>
2016-07-08 18:11:08 +0900

    表示させるべき情報は、ポイント以外はきれいに取れたと思います。 	modified:   ../../plugins/freecycle/lib/reservations.php 	modified:   page_show_reservations.php

--- plugins/freecycle/lib/reservations.php
+++ plugins/freecycle/lib/reservations.php
@@ -35,4 +35,37 @@ function get_bookfair_info_by_bookfair_id($bookfair_id) {
 	return $bookfair_info;
 }
 
+function provide_formatted_books_info() {
+	$whole_info_in_reserve = get_reservation_info_by_current_user_id(); // wp_fmt_reserveテーブルの、ログインID のデータ全て
+	$books_info = array();                                              // 1つの要素は、 1つの本に対応してるよ
+	
+	foreach ($whole_info_in_reserve as $single_book_info) {
+		$post_id    = $single_book_info->item_id;
+		$book_title = get_the_title($post_id);
+
+		// $bookfair_info を展開しときましょう。説明変数にもなるし
+		$bookfair_id   = $single_book_info->bookfair_id;
+		$bookfair_info = get_bookfair_info_by_bookfair_id($bookfair_id);
+		$starting_time = $bookfair_info[0];
+		$ending_time   = $bookfair_info[1];
+		$date          = $bookfair_info[2];
+		$venue         = $bookfair_info[3];
+		$classroom     = $bookfair_info[4];
+		
+		$clear_single_book_info = array($book_title, $date, $starting_time, $ending_time, $venue, $classroom);
+		array_push($books_info, $clear_single_book_info);
+		
+		/* 
+			 サムネイルを取ってこれるはず。。。確認はしてません。ほんと分かりません。
+			 $args = array(
+			 'post_type'   => 'attachment',
+			 'post_parent' => $post_id,
+			 );
+			 $attachment = array_reverse(get_posts($args));
+			 $thumbnail_url[$key] = wp_get_attachment_image_url($attachment->ID, 'thumbnail');
+		 */
+	}
+	return $books_info;
+}
+
 ?>
--- themes/freecycle/page_show_reservations.php
+++ themes/freecycle/page_show_reservations.php
@@ -39,40 +39,10 @@ delete_searchform(get_header());
 				</div>
 
 				<!-- blog-page って領域の中に書いてるんだから、そりゃこいつの初期位置は本文の中になるでしょうよ -->
-				<!--div class="a_reserved_book"></div-->
-				
-				<?php
-				$whole_info_in_reserve = get_reservation_info_by_current_user_id(); // wp_fmt_reserveテーブルの、ログインID のデータ全て
-				$books_info = array();                                              // 1つの要素は、 1つの本に対応してるよ
-				
-				foreach ($whole_info_in_reserve as $single_book_info) {
-					$post_id    = $single_book_info->item_id;
-					$book_title = get_the_title($post_id);
+				<?php /* 本の予約情報の表示部分 */
+				$books_info = provide_formatted_books_info(); // 本の名前, 日時, 開始時刻, 終了時刻, 開催地, 教室
 
-					// $bookfair_info を展開しときましょう。説明変数にもなるし
-					$bookfair_id   = $single_book_info->bookfair_id;
-					$bookfair_info = get_bookfair_info_by_bookfair_id($bookfair_id);
-					$starting_time = $bookfair_info[0];
-					$ending_time   = $bookfair_info[1];
-					$date          = $bookfair_info[2];
-					$venue         = $bookfair_info[3];
-					$classroom     = $bookfair_info[4];
-					
-					$clear_single_book_info = array($book_title, $date, $starting_time, $ending_time, $venue, $classroom);
-					array_push($books_info, $clear_single_book_info);
-					
-					/*
-						 サムネイルを表示させるはず。。。確認はしてません。ほんと分かりません。
-						 $args = array(
-						 'post_type'   => 'attachment',
-						 'post_parent' => $post_id,
-						 );
-						 $attachment = array_reverse(get_posts($args));
-						 $thumbnail_url[$key] = wp_get_attachment_image_url($attachment->ID, 'thumbnail');
-					 */
-				}
-
-				// 情報の表示
+				// FIXME: ちゃんときれいに表示させてください。
 				foreach ($books_info as $key => $single_book_info) {
 					echo "<div class=\"single_reservation\">";
 					echo "本のタイトル: " . $books_info[$key][0];
